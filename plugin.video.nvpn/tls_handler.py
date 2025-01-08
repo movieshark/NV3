d1660fcb3db9d3fafe785544e17abd64043d6c89
MrDini123 <16760748+MrDini123@users.noreply.github.com>
2025-01-08 21:25:15 +0100

    Disclaimer and new cert chain

--- plugin.video.nvpn/tls_handler.py
+++ plugin.video.nvpn/tls_handler.py
@@ -4,6 +4,18 @@ from requests import Session
 from requests.adapters import HTTPAdapter
 
 
+"""
+Disclaimer:
+
+Frankly, this workaround shouldn't exist...
+But the server only supports AES128-SHA ciphers, which is not supported by the default SSL context on the latest Python versions.
+So we have to create a custom SSL context with the required ciphers.
+
+SHA-1 is highly discouraged, but as long as the server doesn't support any other cipher, we have no choice but to use it.
+Your browser would use the same, so it's not less secure than using the site in a browser.
+"""
+
+
 class SSLAdapter(HTTPAdapter):
     def __init__(self, ssl_context=None, **kwargs):
         self.ssl_context = ssl_context
